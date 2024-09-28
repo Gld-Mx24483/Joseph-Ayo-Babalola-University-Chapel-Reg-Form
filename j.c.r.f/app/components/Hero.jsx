55639a852ea399ec30808730ce08404259589add
Gld-Mx24483 <israeltoki7@gmail.com>
2024-09-28 11:38:46 +0100

    "Removed entire Hero component implementation, leaving only imports."

--- j.c.r.f/app/components/Hero.jsx
+++ j.c.r.f/app/components/Hero.jsx
@@ -1,35 +1,4 @@
-// // Hero.jsx
-// import Image from 'next/image';
-// import backgroundImage from '../assets/jabu-auditorium.jpg';
-// import { heroStyles } from './HeroStyles';
-
-// const Hero = () => {
-//   return (
-//     <div className={heroStyles.container}>
-//       <div className={heroStyles.backgroundImageContainer}>
-//         <Image
-//           src={backgroundImage}
-//           alt="JABU Auditorium"
-//           placeholder="blur"
-//           quality={100}
-//           fill
-//           style={{
-//             objectFit: 'cover',
-//           }}
-//         />
-//         <div className={heroStyles.overlay}></div>
-//       </div>
-//       <div className={heroStyles.content}>
-//         <h1 className={heroStyles.title}>JABU CHAPLAINCY UNIT</h1>
-//         <div className={heroStyles.separator}></div>
-//         <p className={heroStyles.subtitle}>STUDENT&#39;S REGISTRATION FORM 2024/25 SESSION</p>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Hero;
-
+// Hero.jsx
 import Image from 'next/image';
 import backgroundImage from '../assets/jabu-auditorium.jpg';
 import { heroStyles } from './HeroStyles';
