9c75b79b6078757c485dc95af4650add6f835e36
Gld-Mx24483 <israeltoki7@gmail.com>
2024-09-28 03:04:15 +0100

    Animations

--- j.c.r.f/app/components/Hero.jsx
+++ j.c.r.f/app/components/Hero.jsx
@@ -1,4 +1,35 @@
-// Hero.jsx
+// // Hero.jsx
+// import Image from 'next/image';
+// import backgroundImage from '../assets/jabu-auditorium.jpg';
+// import { heroStyles } from './HeroStyles';
+
+// const Hero = () => {
+//   return (
+//     <div className={heroStyles.container}>
+//       <div className={heroStyles.backgroundImageContainer}>
+//         <Image
+//           src={backgroundImage}
+//           alt="JABU Auditorium"
+//           placeholder="blur"
+//           quality={100}
+//           fill
+//           style={{
+//             objectFit: 'cover',
+//           }}
+//         />
+//         <div className={heroStyles.overlay}></div>
+//       </div>
+//       <div className={heroStyles.content}>
+//         <h1 className={heroStyles.title}>JABU CHAPLAINCY UNIT</h1>
+//         <div className={heroStyles.separator}></div>
+//         <p className={heroStyles.subtitle}>STUDENT&#39;S REGISTRATION FORM 2024/25 SESSION</p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Hero;
+
 import Image from 'next/image';
 import backgroundImage from '../assets/jabu-auditorium.jpg';
 import { heroStyles } from './HeroStyles';
@@ -20,9 +51,13 @@ const Hero = () => {
         <div className={heroStyles.overlay}></div>
       </div>
       <div className={heroStyles.content}>
-        <h1 className={heroStyles.title}>JABU CHAPLAINCY UNIT</h1>
-        <div className={heroStyles.separator}></div>
-        <p className={heroStyles.subtitle}>STUDENT&#39;S REGISTRATION FORM 2024/25 SESSION</p>
+        <h1 className={`${heroStyles.title} ${heroStyles.fadeInUp}`}>
+          JABU CHAPLAINCY UNIT
+        </h1>
+        <div className={`${heroStyles.separator} ${heroStyles.growWidth}`}></div>
+        <p className={`${heroStyles.subtitle} ${heroStyles.fadeInUp}`}>
+          STUDENT&#39;S REGISTRATION FORM 2024/25 SESSION
+        </p>
       </div>
     </div>
   );
