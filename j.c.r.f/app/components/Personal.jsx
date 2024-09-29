56cd7985b722267de40f4568907ad6a5a76001dc
Gld-Mx24483 <israeltoki7@gmail.com>
2024-09-29 15:08:09 +0100

    deploy

--- j.c.r.f/app/components/Personal.jsx
+++ j.c.r.f/app/components/Personal.jsx
@@ -653,7 +653,7 @@ const Personal = ({ isOpen, onClose, onNext, onPrevious, initialData }) => {
         <button onClick={onClose} className={personalStyles.closeButton}>
           <X size={20} className="sm:w-6 sm:h-6" />
         </button>
-        <h2 className={personalStyles.mainHeading}>STUDENT'S REGISTRATION FORM 2024/25 SESSION</h2>
+        <h2 className={personalStyles.mainHeading}>STUDENT&#39;S REGISTRATION FORM 2024/25 SESSION</h2>
         <h3 className={personalStyles.subHeading}>Personal Section</h3>
         
         <form onSubmit={handleSubmit(onSubmit)} className={personalStyles.form}>
