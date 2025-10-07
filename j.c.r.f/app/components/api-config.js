a185159c651dc0e761eb02c2e9086812a9d72fe9
Adedapo Toki <israeltoki7@gmail.com>
2025-10-07 23:23:14 +0100

    uat branch

--- j.c.r.f/app/components/api-config.js
+++ j.c.r.f/app/components/api-config.js
@@ -1,6 +1,6 @@
 // api-config.js
 const API_URL = process.env.NODE_ENV === 'production' 
   ? 'https://jabu-chapel-form-server.vercel.app' 
-  : 'http://localhost:5000';
+  : 'https://jabu-chapel-form-server.vercel.app';
 
 export default API_URL;
